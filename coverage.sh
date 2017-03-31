e983f65275f206b6a326ee8a14a6985f0d711af5
Ryan Sheftel <rsheftel@gmail.com>
2017-03-31 17:31:48 -0400

    Update for (1) coveralls.io (2) include Python2.7 in the Travis-CI

--- coverage.sh
+++ coverage.sh
@@ -3,4 +3,4 @@
 # This only works in Cygwin on Windows
 
 py3 -m pytest --color=yes --cov=pandas_market_calendars --cov-report html
-cygstart $TEMP/mcal_coverage_report/index.html
+cygstart htmlcov/index.html
