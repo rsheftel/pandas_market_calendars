b7aaa3bcc32bb8b4bb91b6a43955a08ecf169154
Ryan Sheftel <ryan@sheftel.net>
2018-08-20 16:30:50 -0400

    small chanegs prior to the big move to flit

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
