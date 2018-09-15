32d42bc1f258d6255b6ab735e8b8b370226cd91e
woutdenolf <woutdenolf@users.sf.net>
2018-09-15 14:49:53 +0200

    Class registration: adapt tests to reflect alias-only registration.

--- tests/test_class_registry.py
+++ tests/test_class_registry.py
@@ -17,6 +17,7 @@ def test_inheritance():
     factory1 = Class1._regmeta_instance_factory
     
     class Class2(six.with_metaclass(RegisteryMeta,Base)):
+        aliases = ["class 2"]
         def __init__(self,arg0,arg2,kw2=None,**kwargs):
             self.arg2 = arg2
             self.kw2 = kw2
@@ -44,48 +45,28 @@ def test_inheritance():
             self.kw12a = kw12a
             super(Class12a, self).__init__(arg0=arg0,arg1=arg1,arg2=arg2,**kwargs)
     
-    assert set(Class1._regmeta_classes()) == set(['Class1', 'Class1a', 'Class1b', 'Class12a', 'class 1a', 'class 1b', 'class 12a'])
-    assert set(Class2._regmeta_classes()) == set(['Class2', 'Class12a', 'class 12a'])
+    assert set(Class1._regmeta_classes()) == set(['Class1', 'class 1a', 'Class1b', 'class 12a'])
+    assert set(Class2._regmeta_classes()) == set(['class 2', 'class 12a'])
     
-    o = factory1("Class1","0","1",kw0="k0",kw1="k1")
+    o = factory1("class 1","0","1",kw0="k0",kw1="k1")
     assert (o.arg0,o.arg1,o.kw0,o.kw1) == ("0","1","k0","k1")
     assert Class1 == o.__class__
-    
-    o = factory1("Class1a","0","1","a",kw0="k0",kw1="k1",kw1a="k1a")
-    assert (o.arg0,o.arg1,o.arg1a,o.kw0,o.kw1,o.kw1a) == ("0","1","a","k0","k1","k1a")
-    assert Class1a == o.__class__
     
     o = factory1("class 1a","0","1","a",kw0="k0",kw1="k1",kw1a="k1a")
     assert (o.arg0,o.arg1,o.arg1a,o.kw0,o.kw1,o.kw1a) == ("0","1","a","k0","k1","k1a")
     assert Class1a == o.__class__
     
-    o = factory1("Class1b","0","1","b",kw0="k0",kw1="k1",kw1b="k1b")
-    assert (o.arg0,o.arg1,o.arg1b,o.kw0,o.kw1,o.kw1b) == ("0","1","b","k0","k1","k1b")
-    assert Class1b == o.__class__
-    
     o = factory1("class 1b","0","1","b",kw0="k0",kw1="k1",kw1b="k1b")
     assert (o.arg0,o.arg1,o.arg1b,o.kw0,o.kw1,o.kw1b) == ("0","1","b","k0","k1","k1b")
     assert Class1b == o.__class__
-    
-    o = factory1("Class12a","0","1","2","a",kw0="k0",kw1="k1",kw2="k2",kw12a="k12a")
-    assert (o.arg0,o.arg1,o.arg2,o.arg12a,o.kw0,o.kw1,o.kw2,o.kw12a) == ("0","1","2","a","k0","k1","k2","k12a")
-    assert Class12a == o.__class__
     
     o = factory1("class 12a","0","1","2","a",kw0="k0",kw1="k1",kw2="k2",kw12a="k12a")
     assert (o.arg0,o.arg1,o.arg2,o.arg12a,o.kw0,o.kw1,o.kw2,o.kw12a) == ("0","1","2","a","k0","k1","k2","k12a")
     assert Class12a == o.__class__
     
-    o = factory2("Class2","0","2",kw0="k0",kw2="k2")
+    o = factory2("class 2","0","2",kw0="k0",kw2="k2")
     assert (o.arg0,o.arg2,o.kw0,o.kw2) == ("0","2","k0","k2")
     assert Class2 == o.__class__
-    
-    o = factory2("Class12a","0","1","2","a",kw0="k0",kw1="k1",kw2="k2",kw12a="k12a")
-    assert (o.arg0,o.arg1,o.arg2,o.arg12a,o.kw0,o.kw1,o.kw2,o.kw12a) == ("0","1","2","a","k0","k1","k2","k12a")
-    assert Class12a == o.__class__
-    
-    o = factory2("class 12a","0","1","2","a",kw0="k0",kw1="k1",kw2="k2",kw12a="k12a")
-    assert (o.arg0,o.arg1,o.arg2,o.arg12a,o.kw0,o.kw1,o.kw2,o.kw12a) == ("0","1","2","a","k0","k1","k2","k12a")
-    assert Class12a == o.__class__
 
 def test_metamixing():
     BaseMeta = type('BaseMeta', (ABCMeta, RegisteryMeta), {})
@@ -96,7 +77,7 @@ def test_metamixing():
             pass
 
     class Class1(Base):
-        aliases = ['c1']
+        aliases = ['c1','c 1']
         def test(self):
             return 123
 
@@ -107,6 +88,6 @@ def test_metamixing():
     else:
         raise RuntimeError('Abstract class is instantiated')
 
-    o1 = Base._regmeta_instance_factory('Class1')
-    o2 = Base._regmeta_instance_factory('c1')
-    assert o1.test(),o2.test()
+    o1 = Base._regmeta_instance_factory('c1')
+    o2 = Base._regmeta_instance_factory('c 1')
+    assert o1.test() == o2.test()
