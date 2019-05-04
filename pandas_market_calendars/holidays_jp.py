5dd2b092829be2edb066fef706a6ef4f61b02741
misantroop <alepik@gmail.com>
2019-05-05 03:23:42 +0800

    Update holidays_jp.py

--- pandas_market_calendars/holidays_jp.py
+++ pandas_market_calendars/holidays_jp.py
@@ -1,6 +1,4 @@
-from pandas import (
-    Timestamp
-)
+from pandas import date_range
 
 # Apr. 30 (Tue.)    Abdication Day
 # May 1 (Wed.)    Accession Day
@@ -8,10 +6,5 @@ from pandas import (
 # May 3 (Fri.)    Constitution Memorial Day
 # May 4 (Sat.)    Greenery Day
 # May 6 (Mon.)    Children's Day (May 5) observed
-AscensionDays = [
-    Timestamp('2019-04-30', tz='Asia/Tokyo'),
-    Timestamp('2019-05-01', tz='Asia/Tokyo'),
-    Timestamp('2019-05-02', tz='Asia/Tokyo'),
-    Timestamp('2019-05-03', tz='Asia/Tokyo'),
-    Timestamp('2019-05-06', tz='Asia/Tokyo'),
-]
+
+AccessionOfNewEmperor2019 = date_range('2019-04-30', '2019-05-02', tz='UTC')
