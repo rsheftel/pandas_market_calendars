b99ee78574a9fd8098a538b722f96664dc280623
leonarduschen <leonardus.chen@gmail.com>
2020-08-24 23:21:08 +0800

    fix open_at_time

--- pandas_market_calendars/market_calendar.py
+++ pandas_market_calendars/market_calendar.py
@@ -289,7 +289,7 @@ class MarketCalendar(metaclass=MarketCalendarMeta):
             if using bars and would like to include the last bar as a valid open date and time.
         :return: True if the timestamp is a valid open date and time, False if not
         """
-        date = pd.Timestamp(timestamp.date())
+        date = pd.Timestamp(pd.Timestamp(timestamp).tz_convert('UTC').date())
         if date in schedule.index:
             if 'break_start' in schedule.columns:
                 if include_close:
