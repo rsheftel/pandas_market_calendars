f75726dec46d6ec46a857c3bfb0196830f9f973a
Stryder-Git <marcel_pieper@hotmail.com>
2022-04-03 16:55:02 -0400

    created most special_closes of CME Equities, adjusted various holidays, no tests yet

--- pandas_market_calendars/exchange_calendar_cme_globex_equities.py
+++ pandas_market_calendars/exchange_calendar_cme_globex_equities.py
@@ -8,57 +8,64 @@ from pandas import Timestamp
 from pandas.tseries.holiday import AbstractHolidayCalendar, GoodFriday, USLaborDay, USPresidentsDay, USThanksgivingDay
 from pytz import timezone
 
-from .holidays_us import (Christmas, ChristmasEveBefore1993, ChristmasEveInOrAfter1993, USBlackFridayInOrAfter1993,
-                          USIndependenceDay, USMartinLutherKingJrAfter1998, USMemorialDay, USJuneteenthAfter2022,
-                          USNationalDaysofMourning, USNewYearsDay)
-from .market_calendar import MarketCalendar
-
+from pandas_market_calendars.holidays_cme import (
+    USMartinLutherKingJrAfter1998Before2015,
+    USMartinLutherKingJrAfter2015Before2022,
+    USPresidentsDayBefore2015,
+    USPresidentsDayAfter2015,
+    USPresidentsDayBefore2022,
+    GoodFridayBefore2021,
+    GoodFriday2021,
+    GoodFridayAfter2021,
+    USMemorialDay2021AndPrior,
+    USMemorialDay2013AndPrior,
+    USMemorialDay2013To2021,
+    USIndependenceDayBefore2022,
+    USIndependenceDayBefore2022PreviousDay,
+    USIndependenceDayBefore2014,
+    USIndependenceDayAfter2014,
+    USLaborDayStarting1887Before2022,
+    USLaborDayStarting1887Before2014,
+    USLaborDayStarting1887After2014Before2022,
+    USThanksgivingBefore2014,
+    USThanksgivingAfter2014Before2022,
+    USThanksgivingFridayBefore2022,
+)
+from pandas_market_calendars.holidays_us import (
+    USNewYearsDay,
+    ChristmasEveInOrAfter1993,
+    Christmas,
+)
 
 class CMEGlobexEquitiesExchangeCalendar(CMEGlobexBaseExchangeCalendar):
     """
-    Holidays overview
-
-        * Martin Luther King
+        * Martin Luther King  --- done
             2014 and before
                 10:30 early close on the 3rd Monday of January
             after
                 12:00 ''
-
+    """
+    """
         * President's Day
             2014 and before
                 10:30 early close on the 3rd Monday of February
             after
                 12:00 ''
-
-        * Good Friday
-            2009
-                fully closed
-            2010
-                8:15 early close
-            2011
-                fully closed
-            2012
-                8:15 early close
-            2014
-                fully closed
-            2015
-                8:15 early close on the Friday before easter
-            2016 - 2020 (incl)
-                fully closed
-            2021
-                8:15 early close
-
+    """
+    """
         * Memorial Day
             before
                 10:30 early close on the monday
 
-            2014 and after
+            2013 and after
                 12:00 early close on the Monday
-
+    """
+    """
         * Juneteenth
             2022
                 12:00 early close Monday after
-
+    """
+    """
         * Independence Day / July 4th
             2009
                 10:30 early close on 3rd (Fri)
@@ -77,16 +84,15 @@ class CMEGlobexEquitiesExchangeCalendar(CMEGlobexBaseExchangeCalendar):
                 12:15 early close on July 3rd (Thursday)
                 12:00 early close on July 4th (Friday)
             2015
-                12:00 early close on July 3rd (Friday)
+                12:00 early close on July 3rd (Friday)                
             2016
-                12:00 early close on July 4th (Monday)
+                12:00 early close on July 4th (Monday)                
             2017
                 12:15 early close on July 3rd (Monday)
-                12:00 early close on July 4th (Tuesday)
+                12:00 early close on July 4th (Tuesday)                
             2018
                 12:15 early close in 3rd (Tuesday)
                 12:00 early close on 4th (Wed)
-
             2019
                 12:15 early close on 3rd (Wed)
                 12:00 early close on 4th (Thu)
@@ -96,7 +102,18 @@ class CMEGlobexEquitiesExchangeCalendar(CMEGlobexBaseExchangeCalendar):
 
             2021
                 12:00 early close on 5th (Mon)
-
+                
+                
+            holiday (july4th) is the nearest workday
+                on that day, always early close
+            
+            if july 4th is Tue-Fri
+                then july3rd is special early close
+            
+            
+            
+        """
+    """
         * Labor Day
 
             before
@@ -104,7 +121,8 @@ class CMEGlobexEquitiesExchangeCalendar(CMEGlobexBaseExchangeCalendar):
 
             2014 and after
                 12:00 early close on the monday
-
+    """
+    """
         * Thanksgiving
 
             before
@@ -115,15 +133,33 @@ class CMEGlobexEquitiesExchangeCalendar(CMEGlobexBaseExchangeCalendar):
             2014 and after
                 12:00 early close on the Thursday
                 12:15 early close on the Friday after
+    """
+    """
 
-
-        * Christmas
+        * Christmas  --------- GLOBEX
 
                 pretty weird check later
 
-        * New years
+        * New years ----------- GLOBEX
                 also pretty weird
 
+        * Good Friday  ---------- GLOBEX
+            2009
+                fully closed
+            2010
+                8:15 early close
+            2011
+                fully closed
+            2012
+                8:15 early close
+            2014
+                fully closed
+            2015
+                8:15 early close on the Friday before easter
+            2016 - 2020 (incl)
+                fully closed
+            2021
+                8:15 early close
 
 
 
@@ -152,9 +188,17 @@ class CMEGlobexEquitiesExchangeCalendar(CMEGlobexBaseExchangeCalendar):
 
     """
 
+    regular_market_times = {
+        "market_open": ((None, time(17), -1),), # offset by -1 day
+        "market_close": ((None, time(16)),),
+        "break_start": ((None, time(17)),),
+        "break_end": ((None, time(18)),)
+    }
 
     @property
-    @abstractmethod
+    def tz(self): return timezone("America/Chicago")
+
+    @property
     def name(self):
         """
         Name of the market
@@ -163,16 +207,42 @@ class CMEGlobexEquitiesExchangeCalendar(CMEGlobexBaseExchangeCalendar):
         """
         raise NotImplementedError()
 
+    @property
+    def regular_holidays(self):
+        return AbstractHolidayCalendar(rules=[
+            USNewYearsDay,
+            GoodFridayBefore2021,
+            GoodFridayAfter2021,
+            Christmas,
+        ])
 
+    @property
+    def special_closes(self):
+        # Source https://www.cmegroup.com/tools-information/holiday-calendar.html
+        return [
+            (time(10, 30), AbstractHolidayCalendar(rules=[
+                USMartinLutherKingJrAfter1998Before2015,
+                USPresidentsDayBefore2015,
+                USMemorialDay2013AndPrior,
+                USIndependenceDayBefore2014,
+                USLaborDayStarting1887Before2014,
+                USThanksgivingBefore2014,
+            ])),
 
+            (time(12,15), AbstractHolidayCalendar(rules= [
+                USIndependenceDayBefore2022PreviousDay,
+                USThanksgivingFridayBefore2022,
+            ])),
 
+            (time(12), AbstractHolidayCalendar(rules=[
+                USMartinLutherKingJrAfter2015Before2022,
+                USPresidentsDayAfter2015,
+                USMemorialDay2013To2021,
+                USIndependenceDayAfter2014,
+                USLaborDayStarting1887After2014Before2022,
+                USThanksgivingAfter2014Before2022
+            ])),
 
-
-
-
-
-
-
-
+        ]
 
 
--- pandas_market_calendars/holidays_cme.py
+++ pandas_market_calendars/holidays_cme.py
@@ -1,3 +1,5 @@
+import datetime
+
 from dateutil.relativedelta import (MO, FR)
 from dateutil.relativedelta import (TH)
 from pandas import (DateOffset, Timestamp)
@@ -5,6 +7,10 @@ from pandas.tseries.holiday import (Holiday, Easter)
 from pandas.tseries.holiday import (nearest_workday)
 from pandas.tseries.offsets import (Day)
 
+
+#########
+# Martin Luther King
+#########
 USMartinLutherKingJrAfter1998Before2022 = Holiday(
     'Dr. Martin Luther King Jr. Day',
     month=1,
@@ -15,6 +21,28 @@ USMartinLutherKingJrAfter1998Before2022 = Holiday(
     offset=DateOffset(weekday=MO(3)),
 )
 
+USMartinLutherKingJrAfter1998Before2015 = Holiday(
+    'Dr. Martin Luther King Jr. Day',
+    month=1,
+    day=1,
+    # The US markets didn't observe MLK day as a holiday until 1998.
+    start_date=Timestamp('1998-01-01'),
+    end_date=Timestamp('2014-12-31'),
+    offset=DateOffset(weekday=MO(3)),)
+
+USMartinLutherKingJrAfter2015Before2022 = Holiday(
+    'Dr. Martin Luther King Jr. Day',
+    month=1,
+    day=1,
+    # The US markets didn't observe MLK day as a holiday until 1998.
+    start_date=Timestamp('2015-01-01'),
+    end_date=Timestamp('2021-12-31'),
+    offset=DateOffset(weekday=MO(3)),)
+
+
+#########
+# President's Day
+#########
 USPresidentsDayBefore2022 = Holiday(
     'President''s Day',
     start_date=Timestamp('1971-01-01'),
@@ -22,6 +50,23 @@ USPresidentsDayBefore2022 = Holiday(
     month=2, day=1,
     offset=DateOffset(weekday=MO(3)),
 )
+USPresidentsDayBefore2015 = Holiday(
+    'President''s Day',
+    start_date=Timestamp('1971-01-01'),
+    end_date=Timestamp('2014-12-31'),
+    month=2, day=1,
+    offset=DateOffset(weekday=MO(3)),)
+USPresidentsDayAfter2015 = Holiday(
+    'President''s Day',
+    start_date=Timestamp('2015-01-01'),
+    end_date=Timestamp('2021-12-31'),
+    month=2, day=1,
+    offset=DateOffset(weekday=MO(3)),)
+
+
+#########
+# Good Friday
+#########
 
 GoodFridayBefore2021 = Holiday(
     "Good Friday",
@@ -43,6 +88,10 @@ GoodFridayAfter2021 = Holiday(
     start_date=Timestamp('2022-01-01'),
 )
 
+#########
+# Memorial Day
+#########
+
 USMemorialDay2021AndPrior = Holiday(
     'Memorial Day',
     month=5,
@@ -50,7 +99,27 @@ USMemorialDay2021AndPrior = Holiday(
     start_date=Timestamp('1971-01-01'),
     end_date=Timestamp('2021-12-31'),
     offset=DateOffset(weekday=MO(1)),
+)#### Equity Products
+USMemorialDay2013AndPrior = Holiday(
+    'Memorial Day',
+    month=5,
+    day=25,
+    start_date=Timestamp('1971-01-01'),
+    end_date=Timestamp('2013-12-31'),
+    offset=DateOffset(weekday=MO(1)),
 )
+USMemorialDay2013To2021 = Holiday(
+    'Memorial Day',
+    month=5,
+    day=25,
+    start_date=Timestamp('2014-01-01'),
+    end_date=Timestamp('2022-01-01'),
+    offset=DateOffset(weekday=MO(1)),
+)
+
+#######
+# Independence Day
+#######
 
 USIndependenceDayBefore2022 = Holiday(
     'July 4th',
@@ -58,8 +127,41 @@ USIndependenceDayBefore2022 = Holiday(
     day=4,
     start_date=Timestamp('1954-01-01'),
     end_date=Timestamp('2021-12-31'),
-    observance=nearest_workday,
+    observance=nearest_workday,)
+USIndependenceDayBefore2014 = Holiday(
+    'July 4th',
+    month=7,
+    day=4,
+    start_date=Timestamp('1954-01-01'),
+    end_date=Timestamp('2013-12-31'),
+    observance=nearest_workday,)
+USIndependenceDayAfter2014 = Holiday(
+    'July 4th',
+    month=7,
+    day=4,
+    start_date=Timestamp('2014-01-01'),
+    end_date=Timestamp('2021-12-31'),
+    observance=nearest_workday,)
+
+def previous_workday_if_july_4th_is_tue_to_fri(dt):
+    july4th = datetime.datetime(dt.year, 7, 4)
+    if july4th.weekday() in (1, 2, 3, 4):
+        return july4th - datetime.timedelta(days= 1)
+    # else None
+
+USIndependenceDayBefore2022PreviousDay = Holiday(
+    'July 4th',
+    month=7,
+    day=4,
+    start_date=Timestamp('1954-01-01'),
+    end_date=Timestamp('2021-12-31'),
+    observance= previous_workday_if_july_4th_is_tue_to_fri
 )
+
+
+#########
+# Labor Day
+#########
 
 USLaborDayStarting1887Before2022 = Holiday(
     "Labor Day",
@@ -69,7 +171,27 @@ USLaborDayStarting1887Before2022 = Holiday(
     end_date=Timestamp('2021-12-31'),
     offset=DateOffset(weekday=MO(1))
 )
+USLaborDayStarting1887Before2014 = Holiday(
+    "Labor Day",
+    month=9,
+    day=1,
+    start_date=Timestamp("1887-01-01"),
+    end_date=Timestamp('2013-12-31'),
+    offset=DateOffset(weekday=MO(1))
+)
+USLaborDayStarting1887After2014Before2022 = Holiday(
+    "Labor Day",
+    month=9,
+    day=1,
+    start_date=Timestamp("2014-01-01"),
+    end_date=Timestamp('2021-12-31'),
+    offset=DateOffset(weekday=MO(1))
+)
 
+
+#########
+# Thanksgiving
+#########
 
 USThanksgivingBefore2022 = Holiday(
     'ThanksgivingFriday',
@@ -78,21 +200,67 @@ USThanksgivingBefore2022 = Holiday(
     month=11, day=1,
     offset=DateOffset(weekday=TH(4)),
 )
+USThanksgivingBefore2014 = Holiday(
+    'ThanksgivingFriday',
+    start_date=Timestamp('1942-01-01'),
+    end_date=Timestamp('2013-12-31'),
+    month=11, day=1,
+    offset=DateOffset(weekday=TH(4)),
+)
+USThanksgivingAfter2014Before2022 = Holiday(
+    'ThanksgivingFriday',
+    start_date=Timestamp('2014-01-01'),
+    end_date=Timestamp('2021-12-31'),
+    month=11, day=1,
+    offset=DateOffset(weekday=TH(4)),
+)
+
+######## The following Holidays shouldn't be set with the FR offset
+## In 2013, Nov 1st is a friday, so the 4th Friday is before the 4th Thursday...
+# the observance rule defined herafter fixes this
+#
+# USThanksgivingFridayBefore2022 = Holiday(
+#     'ThanksgivingFriday',
+#     start_date=Timestamp('1942-01-01'),
+#     end_date=Timestamp('2021-12-31'),
+#     month=11, day=1,
+#     offset=DateOffset(weekday=FR(4)),
+# )
+#
+# USThanksgivingFriday2022AndAfter = Holiday(
+#     'ThanksgivingFriday',
+#     start_date=Timestamp('2022-01-01'),
+#     month=11, day=1,
+#     offset=DateOffset(weekday=FR(4)),
+# )
+# USThanksgivingFriday = Holiday(
+#     'ThanksgivingFriday',
+#     month=11, day=1,
+#     offset=DateOffset(weekday=FR(4)),
+# )
+
+def fri_after_4th_thu(dt):
+    # dt will just be Nov 1st
+    diff_to_thu = 3 - dt.weekday()
+    if diff_to_thu < 0: diff_to_thu += 7
+    return dt + datetime.timedelta(days=diff_to_thu + 22)
+
 USThanksgivingFridayBefore2022 = Holiday(
     'ThanksgivingFriday',
     start_date=Timestamp('1942-01-01'),
     end_date=Timestamp('2021-12-31'),
     month=11, day=1,
-    offset=DateOffset(weekday=FR(4)),
+    observance= fri_after_4th_thu,
 )
+
 USThanksgivingFriday2022AndAfter = Holiday(
     'ThanksgivingFriday',
     start_date=Timestamp('2022-01-01'),
     month=11, day=1,
-    offset=DateOffset(weekday=FR(4)),
+    observance= fri_after_4th_thu,
 )
 USThanksgivingFriday = Holiday(
     'ThanksgivingFriday',
     month=11, day=1,
-    offset=DateOffset(weekday=FR(4)),
+    observance= fri_after_4th_thu,
 )
