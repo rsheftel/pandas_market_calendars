a78960cfcab27caaf8cfb99824864b122d0da586
Ryan Sheftel <ryan@sheftel.net>
2023-12-30 22:11:51 -0500

    Another format with Black

--- tests/test_market_calendar.py
+++ tests/test_market_calendar.py
@@ -332,13 +332,13 @@ def test_add_change_remove_time_w_open_close_map():
     ## Non standard time
 
     cal.add_time("newtime", time(10))
-    assert not "newtime" in cal.open_close_map and "newtime" in cal.regular_market_times
+    assert "newtime" not in cal.open_close_map and "newtime" in cal.regular_market_times
 
     cal.remove_time("newtime")
     assert "newtime" not in cal.regular_market_times
 
     cal.add_time("newtime", time(10), opens=None)
-    assert not "newtime" in cal.open_close_map and "newtime" in cal.regular_market_times
+    assert "newtime" not in cal.open_close_map and "newtime" in cal.regular_market_times
 
     cal.change_time("newtime", time(11), opens=False)
     assert cal.open_close_map["newtime"] is False
@@ -353,8 +353,8 @@ def test_add_change_remove_time_w_open_close_map():
 
     cal.remove_time("market_close")
     assert (
-        not "market_close" in cal.open_close_map
-        and not "market_close" in cal.regular_market_times
+            "market_close" not in cal.open_close_map
+            and "market_close" not in cal.regular_market_times
     )
 
     cal.add_time("market_close", time(15))
@@ -366,7 +366,7 @@ def test_add_change_remove_time_w_open_close_map():
     cal.remove_time("market_close")
     cal.add_time("market_close", time(15), opens=None)
     assert (
-        not "market_close" in cal.open_close_map
+            "market_close" not in cal.open_close_map
         and "market_close" in cal.regular_market_times
     )
 
@@ -395,7 +395,7 @@ def test_open_close_map():
         "pre": True,
         "post": False,
     }
-    assert not cal.open_close_map is FakeCalendar.open_close_map
+    assert cal.open_close_map is not FakeCalendar.open_close_map
     assert cal.open_close_map == FakeCalendar.open_close_map
 
     class WrongCal(FakeCalendar):
@@ -427,7 +427,7 @@ def test_dunder_methods():
 
 
 def test_default_calendars():
-    for name in filter(lambda n: not n[:4] in ("Test", "_Tst"), get_calendar_names()):
+    for name in filter(lambda n: n[:4] not in ("Test", "_Tst"), get_calendar_names()):
         # XKRX has discontinued market times, which should raise a warning
         if name == "XKRX":
             with pytest.warns(UserWarning):
