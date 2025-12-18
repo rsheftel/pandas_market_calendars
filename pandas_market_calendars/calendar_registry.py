05e68d88f4f06e55bf88e80eddcc2a88b64ffd03
Ryan Sheftel <ryan@sheftel.net>
2025-12-18 11:55:51 -0500

    Bump version to 5.2.2 to fix the calendar_registry.py that was improperly formatted with black

--- pandas_market_calendars/calendar_registry.py
+++ pandas_market_calendars/calendar_registry.py
@@ -1,5 +1,44 @@
 # fmt: off
+# @formatter:off
+from .market_calendar import MarketCalendar
+from .calendars.asx import ASXExchangeCalendar
+from .calendars.bmf import BMFExchangeCalendar
+from .calendars.bse import BSEExchangeCalendar
+from .calendars.cboe import CFEExchangeCalendar
+from .calendars.cme import CMEEquityExchangeCalendar, CMEBondExchangeCalendar
+from .calendars.cme_globex_base import CMEGlobexBaseExchangeCalendar
+from .calendars.cme_globex_agriculture import (
+    CMEGlobexAgricultureExchangeCalendar,
+    CMEGlobexLivestockExchangeCalendar,
+    CMEGlobexGrainsAndOilseedsExchangeCalendar,
+)
+from .calendars.cme_globex_crypto import CMEGlobexCryptoExchangeCalendar
+from .calendars.cme_globex_energy_and_metals import (
+    CMEGlobexEnergyAndMetalsExchangeCalendar,
+)
+from .calendars.cme_globex_equities import CMEGlobexEquitiesExchangeCalendar
+from .calendars.cme_globex_fx import CMEGlobexFXExchangeCalendar
+from .calendars.cme_globex_fixed_income import CMEGlobexFixedIncomeCalendar
+from .calendars.eurex import EUREXExchangeCalendar
+from .calendars.eurex_fixed_income import EUREXFixedIncomeCalendar
+from .calendars.hkex import HKEXExchangeCalendar
+from .calendars.ice import ICEExchangeCalendar
+from .calendars.iex import IEXExchangeCalendar
+from .calendars.jpx import JPXExchangeCalendar
+from .calendars.lse import LSEExchangeCalendar
+from .calendars.nyse import NYSEExchangeCalendar
+from .calendars.ose import OSEExchangeCalendar
+from .calendars.sifma import (
+    SIFMAUSExchangeCalendar,
+    SIFMAUKExchangeCalendar,
+    SIFMAJPExchangeCalendar,
+)
+from .calendars.six import SIXExchangeCalendar
+from .calendars.sse import SSEExchangeCalendar
+from .calendars.tase import TASEExchangeCalendar
+from .calendars.tsx import TSXExchangeCalendar
 from .calendars.mirror import *
+# @formatter:on
 # fmt: on
 
 
