6556f5dd3e4e81a1df88273c3643e00f22de59b5
wiktorkisielewski <wiktor@kisielewski.dev>
2026-01-07 21:23:38 +0100

    Add forex calendar

---
+++ pandas_market_calendars/calendars/forex.py
@@ -0,0 +1,34 @@
+from datetime import time
+
+from zoneinfo import ZoneInfo
+
+from pandas_market_calendars.market_calendar import MarketCalendar
+
+
+class ForexExchangeCalendar(MarketCalendar):
+    """
+    Exchange calendar for Forex (FX) market.
+
+    Trading hours: Sunday 5:00 PM EST/EDT to Friday 5:00 PM EST/EDT.
+    Closed on Saturdays, no holidays.
+    """
+
+    aliases = ["FOREX", "FX", "Forex"]
+
+    regular_market_times = {
+        "market_open": ((None, time(17, 0), -1),),
+        "market_close": ((None, time(17, 0)),),
+    }
+
+    @property
+    def name(self):
+        return "FOREX"
+
+    @property
+    def tz(self):
+        return ZoneInfo("America/New_York")
+
+    @property
+    def weekmask(self):
+        return "Sun Mon Tue Wed Thu Fri"
+
