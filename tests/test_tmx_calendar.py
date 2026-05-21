5f9cb1fa63fbf2d29048b2278e5bff7d853f9d61
Luke Sewell <dev@lddsewell.co.uk>
2026-05-21 16:13:07 +0100

    Add Montreal exchange

---
+++ tests/test_tmx_calendar.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pandas.testing import assert_index_equal
+from zoneinfo import ZoneInfo
+
+from pandas_market_calendars.calendars.tmx import (
+    MonExBondExchangeCalendar, MonExIndexExchangeCalendar, MonExStirExchangeCalendar
+)
+
+
+def test_time_zone():
+    assert MonExBondExchangeCalendar().tz == ZoneInfo("Canada/Eastern")
+    assert MonExIndexExchangeCalendar().tz == ZoneInfo("Canada/Eastern")
+    assert MonExStirExchangeCalendar().tz == ZoneInfo("Canada/Eastern")
+    
+
+
+def test_rememberance_day():
+    cal = MonExBondExchangeCalendar()
+    sched_2018 = cal.schedule("2018-10-25", "2018-11-25")
+    assert sched_2018.loc[pd.Timestamp("2018-11-09"), "market_close"] == pd.Timestamp("2018-11-09 18:30 +0")
+    assert pd.Timestamp("2018-11-12") not in sched_2018.index.get_level_values(0)
+
