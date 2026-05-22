92f0b192518ebae9bc7cfcc4d7fe9f202c13763c
Luke Sewell <dev@lddsewell.co.uk>
2026-05-22 11:17:04 +0100

    Improve test coverage

--- tests/test_tmx_calendar.py
+++ tests/test_tmx_calendar.py
@@ -14,9 +14,17 @@ def test_time_zone():
     
 
 
-def test_rememberance_day():
+def test_rememberance_day_closed_bonds():
     cal = MonExBondExchangeCalendar()
     sched_2018 = cal.schedule("2018-10-25", "2018-11-25")
     assert sched_2018.loc[pd.Timestamp("2018-11-09"), "market_close"] == pd.Timestamp("2018-11-09 18:30 +0")
     assert pd.Timestamp("2018-11-12") not in sched_2018.index.get_level_values(0)
+    
+
+
+def test_rememberance_day_open_index():
+    cal = MonExIndexExchangeCalendar()
+    sched_2018 = cal.schedule("2018-10-25", "2018-11-25")
+    assert sched_2018.loc[pd.Timestamp("2018-11-09"), "market_close"] == pd.Timestamp("2018-11-09 21:30 +0")
+    assert pd.Timestamp("2018-11-12") in sched_2018.index.get_level_values(0)
 
